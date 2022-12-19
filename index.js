1762279a2897829865db8b7967de346c929070fb
Umarbek Rakhmatov <umarbek1306@gmail.com>
2022-12-19 11:52:25 -0500

    commit tree 11710f3678cc2f63b444c49005bdac6a22f5aa65

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const main = document.getElementById("main")
+main.remove();
+
+const newHeader = document.createElement("h1");newHeader.setAttribute("id", "victory")
+newHeader.textContent = "Dan is the champion";
