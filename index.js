dba460496317b18e5f36fc5bf1f348ec2b60fb59
Umarbek Rakhmatov <umarbek1306@gmail.com>
2023-01-17 09:53:58 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.querySelector('#main')
+main.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = "Omar is the champion"
